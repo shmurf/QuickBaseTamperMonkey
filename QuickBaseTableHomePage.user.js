2d52f340305456ea149b91c72d03bdb9a5fdf7a4
shmurf <shmurf@users.noreply.github.com>
2023-09-11 11:18:50 -0400

    Create QuickBaseTableHomePage.user.js

---
+++ QuickBaseTableHomePage.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         QuickBase Return To Table Home Page
+// @namespace    QuickBaseReturnToTableHomePage
+// @version      0.1
+// @description  Redirect from table settings to table home page regardless of origin
+// @author       SimonH
+// @match        ://*.quickbase.com/db/*a=listfields*
+// @match        ://*.quickbase.com/db/*a=Relationships*
+// @match        ://*.quickbase.com/db/*a=KeyProps*
+// @match        ://*.quickbase.com/db/*a=tablehomepagesettings*
+// @match        ://*.quickbase.com/db/*a=reportList*
+// @match        ://*.quickbase.com/db/*a=dformList*
+// @match        ://*.quickbase.com/db/*a=EmailList*
+// @match        ://*.quickbase.com/db/*a=TablePermissions*
+// @match        ://*.quickbase.com/db/*a=WebhookList*
+// @match        ://*.quickbase.com/db/*a=QuickBaseActionList*
+// @grant        none
+// @updateURL    https://github.com/jtorrence0/QuickBaseTamperMonkey/raw/master/QuickBaseTableHomePage.user.js
+// ==/UserScript==
+
+let $ = window.jQuery;
+let exitSettings = document.getElementById("pageNavBarExitSettingsAction");
+
+// download button
+let homeButton = document.createElement("a");
+homeButton.classList.add("Settings");
+homeButton.onclick = tableHome;
+homeButton.innerText = "Table HomePage";
+
+exitSettings.appendChild(homeButton);
+
+function tableHome() {
+  window.location.search = "a=td";
+    }
